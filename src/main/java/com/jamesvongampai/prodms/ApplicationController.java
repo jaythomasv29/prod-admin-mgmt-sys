893a3018e70e7934210d72a8c02c0210f127758b
jamesvongampai <jaythomasv29@gmail.com>
2022-10-23 13:40:53 -0700

    add template files for layout building

--- src/main/java/com/jamesvongampai/prodms/ApplicationController.java
+++ src/main/java/com/jamesvongampai/prodms/ApplicationController.java
@@ -7,12 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ApplicationController {
   @GetMapping("/")
   public String getIndex() {
-    return "pages-register";
+    return "index";
   }
 
   @GetMapping("/register")
-  public String getRegister() { return "pages-register"; };
+  public String getRegister() { return "pages-register"; }
+
+  @GetMapping("/hr")
+  public String getHumanResourceIndex() { return "/hr/index"; }
+
+
+  @GetMapping("/accounts")
+  public String getAccountsIndex() { return "/accounts/index"; }
+
+
+
+  @GetMapping("/products")
+  public String getProductsIndex() { return "/products/index"; }
+
+  @GetMapping("/employees")
+  public String getPayrollIndex() { return "/employees/index"; }
 
 }
+
 
 
